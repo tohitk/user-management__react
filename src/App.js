8e1b4c61bc16c9f033b9ab0d8620099768917e2b
Tohit Khan <tohitkhan.cr7@gmail.com>
2022-01-06 18:01:28 +0530

    error modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  function formSumbitHandler(uName, uAge) {
+    setUserList((prevState) => {
+      return [
+        { name: uName, age: uAge, id: Math.random().toString() },
+        ...prevState,
+      ];
+    });
+  }
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={formSumbitHandler} />
+      <UsersList users={userList} />
     </div>
   );
 }
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
-import Button from "../UI/Button";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [isError, setIsError] = useState(false);
+
   function formSumbitHandler(event) {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+      setIsError(true);
+    if (+enteredAge < 1) setIsError(true);
+
+    props.onAddUser(enteredUsername, enteredAge);
+
     setEnteredUsername("");
     setEnteredAge("");
+  }
+
+  function removeModalHandler() {
+    setIsError(false);
   }
 
   function usernameChangeHandler(event) {
@@ -22,25 +34,35 @@ const AddUser = () => {
   }
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={formSumbitHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
+    <div>
+      {isError && (
+        <ErrorModal
+          title="An error occured"
+          message="Something went wrong"
+          onClick={removeModalHandler}
         />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          type="number"
-          id="age"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="sumbit">Add User</Button>
-      </form>
-    </Card>
+      )}
+
+      <Card className={classes.input}>
+        <form onSubmit={formSumbitHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="sumbit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
